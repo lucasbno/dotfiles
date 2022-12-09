5ab33c1dac31ab08f295a5f120a4e127dcc3a355
Lucas Bueno <lucasbnodev@gmail.com>
2022-12-09 16:27:34 -0300

    colors

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -39,6 +39,7 @@ theme.aqua = '#7bd9e6'
 -- backgrounds
 -- theme.bg_normal     = "#0d0f18"
 theme.bg_normal     = "#090B0D"
+-- theme.bg_normal     = "#101419"
 theme.bg_contrast   = "#0f111a"
 theme.bg_lighter    = "#11131c"
 
@@ -78,35 +79,12 @@ theme.tray_chevron_down = gears.color.recolor_image(assets_path .. 'tray/down.sv
 theme.taglist_fg_focus = "#50fa7b"
 theme.taglist_font = "Inter 15"
 
-
 -- menu
 theme.menu_font = theme.font
 theme.menu_submenu_icon = gears.color.recolor_image(shapes_path .. "triangle.png", theme.fg_normal)
 theme.menu_height = dpi(40)
 theme.menu_width = dpi(180)
 theme.menu_bg_focus = theme.bg_lighter
-
--- titlebar
-theme.titlebar_bg = theme.bg_contrast
-theme.titlebar_bg_focus = theme.bg_normal
-theme.titlebar_fg = theme.fg_normal
-
--- close
-theme.titlebar_close_button_normal = gears.color.recolor_image(titlebar_assets_path .. "circle.png", theme.dimblack)
-theme.titlebar_close_button_focus = gears.color.recolor_image(titlebar_assets_path .. "circle.png", theme.red)
-
--- maximized
-theme.titlebar_maximized_button_normal_active = gears.color.recolor_image(titlebar_assets_path .. "circle.png", theme.dimblack)
-theme.titlebar_maximized_button_normal_inactive = gears.color.recolor_image(titlebar_assets_path .. "circle.png", theme.dimblack)
-theme.titlebar_maximized_button_focus_active = gears.color.recolor_image(titlebar_assets_path .. "circle.png", theme.yellow)
-theme.titlebar_maximized_button_focus_inactive = gears.color.recolor_image(titlebar_assets_path .. "circle.png", theme.yellow)
-
--- minimize
-theme.titlebar_minimize_button_normal = gears.color.recolor_image(titlebar_assets_path .. "circle.png", theme.dimblack)
-theme.titlebar_minimize_button_focus = gears.color.recolor_image(titlebar_assets_path .. "circle.png", theme.green)
-
--- wallpaper
-theme.wallpaper = assets_path .. "wallpaper.jpg"
 
 -- layouts
 theme.layout_fairh = gears.color.recolor_image(themes_path.."default/layouts/fairhw.png", theme.fg_normal)
@@ -136,42 +114,10 @@ theme.poweroff_icon = icons_path .. 'poweroff.svg'
 theme.volume_on = gears.color.recolor_image(icons_path .. 'volume-on.svg', theme.fg_normal)
 theme.volume_muted = gears.color.recolor_image(icons_path .. 'volume-muted.svg', theme.fg_normal)
 
-theme.network_connected = ''
-theme.network_disconnected = '睊'
-
--- pfp
-theme.pfp = assets_path .. 'pfp.png'
-
--- fallback music
-theme.fallback_music = assets_path .. 'fallback-music.png'
-
 -- fallback notification icon
 theme.fallback_notif_icon = gears.color.recolor_image(icons_path .. 'hints.svg', theme.blue)
 
 -- disable icon theme
 theme.icon_theme = nil
-
--- task preview
-theme.task_preview_widget_border_radius = dpi(7)
-theme.task_preview_widget_bg = theme.bg_normal
-theme.task_preview_widget_border_color = theme.bg_normal
-theme.task_preview_widget_border_width = 0
-theme.task_preview_widget_margin = dpi(10)
-
--- tag preview
-theme.tag_preview_widget_border_radius = dpi(7)
-theme.tag_preview_client_border_radius = dpi(7)
-theme.tag_preview_client_opacity = 0.5
-theme.tag_preview_client_bg = theme.bg_lighter
-theme.tag_preview_client_border_color = theme.blue
-theme.tag_preview_client_border_width = 1
-theme.tag_preview_widget_bg = theme.bg_normal
-theme.tag_preview_widget_border_color = theme.bg_normal
-theme.tag_preview_widget_border_width = 0
-theme.tag_preview_widget_margin = dpi(7)
-
--- tooltip
-theme.tooltip_bg = theme.bg_normal
-theme.tooltip_fg = theme.fg_normal
 
 return theme
