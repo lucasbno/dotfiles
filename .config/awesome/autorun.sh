4059e776c5526d9cd3c9abaa317fadfb1f1db3c7
Lucas <lucasbnodev@gmail.com>
2023-03-21 14:56:07 -0300

    awesomewm autorun and vscode settingsd

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -5,6 +5,6 @@
 xset r rate 220 40 &
 xmodmap ~/.Xmodmap &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-picom --config ~/.config/picom/picom-pijulius.conf &
+picom --config ~/.config/picom/picom-jonaburg.conf &
 xinput set-prop 'Logitech G403 Prodigy Gaming Mouse' 'libinput Accel Profile Enabled' 0 1 &
 xinput set prop 'Logitech G403 Prodigy Gaming Mouse' 'libinput Accel Speed' 0.7
