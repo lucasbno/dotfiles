5cc378dc3d6b9677ea817d57d8aa33ac89c1dd96
Lucas <lucasbno@proton.me>
2023-05-02 15:25:12 -0300

    fix alacritty title

--- .config/awesome/keys.lua
+++ .config/awesome/keys.lua
@@ -3,11 +3,33 @@ terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
+local function view_nonempty(i, screen)
+        screen = screen or awful.screen.focused()
+        local tags = screen.tags
+        local showntags = {}
+        for _, t in ipairs(tags) do
+                if not awful.tag.getproperty(t, "hide") then
+                        table.insert(showntags, t)
+                end
+        end
+        local sel = screen.selected_tag
+        -- Up to now, this is just copy&paste from awful.tag.viewidx().
+        -- The rest is different
+        local t = gears.table.cycle_value(showntags, sel, nil, function(t)
+                return #t:clients() > 0
+        end)
+        if t then
+                awful.tag.viewnone(screen)
+                t.selected = true
+                screen:emit_signal("tag::history::update")
+        end
+end
+
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+  awful.button({}, 3, function() mymainmenu:toggle() end),
+  awful.button({}, 4, awful.tag.viewnext),
+  awful.button({}, 5, awful.tag.viewprev)
 ))
 -- }}}
 
@@ -26,9 +48,8 @@ globalkeys = gears.table.join(
   awful.key({ modkey }, 'Up', function() awful.client.incwfact(0.05) end),
   awful.key({ modkey }, 'Down', function() awful.client.incwfact(-0.05) end),
   awful.key({ modkey }, 'BackSpace', awful.tag.history.restore),
-  awful.key({}, 'F23', awful.tag.history.restore),
-  --awful.key({}, 'F24', awful.tag.viewnext),
-  awful.key({}, 'F24', function() awful.client.focus.byidx(1) end),
+  awful.key({}, 'F23', function() view_nonempty() end),
+  awful.key({}, 'F24', awful.tag.history.restore),
 
   -- Applications
   --awful.key({ modkey }, 'e', function() awful.spawn(terminal .. " -e ranger") end),
@@ -75,28 +96,30 @@ clientkeys = gears.table.join(
 )
 
 for i = 1, 9 do
-    globalkeys = gears.table.join(globalkeys,
-        -- View tag only.
-        awful.key({ modkey }, "#" .. i + 9,
-                  function ()
-                        local screen = awful.screen.focused()
-                        local tag = screen.tags[i]
-                        if tag then
-                           tag:view_only()
-                        end
-                  end),
-        -- Move client to tag.
-        awful.key({ modkey, "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = client.focus.screen.tags[i]
-                          if tag then
-                              client.focus:move_to_tag(tag)
-                          end
-                     end
-                  end)
-    )
+  globalkeys = gears.table.join(globalkeys,
+    -- View tag only.
+    awful.key({ modkey }, "#" .. i + 9,
+      function()
+        local screen = awful.screen.focused()
+        local tag = screen.tags[i]
+        if tag then
+          tag:view_only()
+        end
+      end),
+    -- Move client to tag.
+    awful.key({ modkey, "Shift" }, "#" .. i + 9,
+      function()
+        if client.focus then
+          local tag = client.focus.screen.tags[i]
+          if tag then
+            client.focus:move_to_tag(tag)
+          end
+        end
+      end)
+  )
 end
+
+-- Cycle only through non-empty tags
 
 clientbuttons = gears.table.join(
   awful.button({}, 1, function(c) client.focus = c end),
