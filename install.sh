492da56cd51db4088d0a8498f88733bbbb302e68
Lucas Bueno <lucasbnodev@gmail.com>
2023-05-20 22:38:19 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-packages_url="https://raw.githubusercontent.com/LucasBno/dotfiles/main/packages"
-packages=$(curl "${packages_url}")
-echo "Installing packages"
-sudo xbps-install -S $packages
 
-echo "Finished"
+install_packages(){
+  packages_url="https://raw.githubusercontent.com/LucasBno/dotfiles/main/packages"
+  packages=$(curl "${packages_url}")
+
+  echo "Installing packages"
+  sleep 2
+  sudo xbps-install -S $packages
+
+  sleep 1
+  echo "Finished downloading packages"
+}
+
+
+install_awesome() {
+  echo "Installing awesomewm"
+  sleep 1
+
+  git clone https://github.com/awesomeWM/awesome.git
+  cd awesome
+
+  make
+  sudo make install
+
+  cd ..
+  rm -rf awesome
+
+  echo "Finished awesomewm installation"
+}
+
+install_packages
+
+read -p "Should install Awesomewm? (y/n)" answer
+if [ "$answer" != "${answer#[Yy]}" ]; then
+  install_awesome
+else
+  echo "Awesomewm installation canceled"
+fi
