e5b972b13bfbc62d77bea4b2f7335dc73965695e
Lucas Bueno <lucasbnodev@gmail.com>
2023-05-20 23:00:57 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Verificar se o script está sendo executado diretamente
-if [ "$$" -ne 1 ]; then
-  echo "O script não está sendo executado diretamente. Ignorando a execução da função."
-  exit 0
-fi
-
 # Função para instalar os pacotes
 install_packages() {
   # Seu código para instalar pacotes
