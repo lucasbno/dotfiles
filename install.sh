b23fdafb76d9cdc2cf4d68bc22c69d2a7d4f9cee
Lucas Bueno <lucasbnodev@gmail.com>
2023-05-20 20:44:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 packages_url="https://raw.githubusercontent.com/LucasBno/dotfiles/main/packages"
-
 packages=$(curl "${packages_url}")
-
+echo "Installing packages"
 sudo xbps-install -S $packages
+
+echo "Finished"
