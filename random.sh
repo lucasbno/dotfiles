85e3a4d2d74f7f445392992f8a0b9980e882ac1a
Lucas Bueno <lucasbnodev@gmail.com>
2023-05-20 20:40:52 +0000

    Update random.sh

--- random.sh
+++ random.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo xbps-install -S $(cat ~/packages)
+packages_url="https://raw.githubusercontent.com/LucasBno/dotfiles/main/packages"
+
+packages=$(curl "${packages_url}")
+
+sudo xbps-install -S $packages
