c5dd1d58d915b581a475701d7097c1770b924759
Lucas Bueno <lucasbno@proton.me>
2023-06-17 22:11:10 -0300

    dotfiles

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -7,6 +7,6 @@ xmodmap ~/.Xmodmap &
 xcape -e 'Hyper_L=Escape' -t 100 &
 # $HOME/.config/awesome/scripts/audio &
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-picom & 
+# picom & 
 xinput set-prop 'Logitech G403 Prodigy Gaming Mouse' 'libinput Accel Profile Enabled' 0 1 &
 xinput set prop 'Logitech G403 Prodigy Gaming Mouse' 'libinput Accel Speed' 0.7
