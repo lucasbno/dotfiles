2fa7c637fe0ca55965cce2a7e7d1b076ce649e3e
Lucas Bueno <lucasbno@proton.me>
2023-09-27 20:14:19 -0300

    Automatic commit script

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -1,30 +1,30 @@
 lvim.plugins = {
-  {
-    "max397574/better-escape.nvim",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
-  "catppuccin/nvim",
-  "kylechui/nvim-surround",
-  "andweeb/presence.nvim",
-  "simrat39/rust-tools.nvim,
-  {
-    {
-    "saecki/crates.nvim",
-    version = "v0.3.0",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("crates").setup {
-        null_ls = {
-          enabled = true,
-          name = "crates.nvim",
-        },
-        popup = {
-          border = "rounded",
-        },
-      }
-    end,
-  },
-  }
-  }
+	{
+		"max397574/better-escape.nvim",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
+	"catppuccin/nvim",
+	"kylechui/nvim-surround",
+	"andweeb/presence.nvim",
+	"simrat39/rust-tools.nvim",
+	{
+		{
+			"saecki/crates.nvim",
+			version = "v0.3.0",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			config = function()
+				require("crates").setup({
+					null_ls = {
+						enabled = true,
+						name = "crates.nvim",
+					},
+					popup = {
+						border = "rounded",
+					},
+				})
+			end,
+		},
+	},
+}
