1233773d68545eec8315ef1a297b6dcd557ea526
Lucas Bueno <lucasbno@proton.me>
2024-06-03 08:04:24 -0300

    scripts

--- .config/zsh/utils.sh
+++ .config/zsh/utils.sh
@@ -2,9 +2,36 @@ fzf-commit() {
   git log --oneline | fzf | cut -d' ' -f1 | copy
 }
 
-git-branch() {
+git-checkout() {
   BRANCH=$(git branch | cut -c 3- | gum choose)
   git checkout $BRANCH
+}
+
+git-update() {
+  BRANCH=$(git branch | cut -c 3- | gum choose)
+
+  git fetch upstream
+
+  git checkout $BRANCH
+
+  git merge upstream/$BRANCH 
+
+  git push origin
+}
+
+git-download() {
+  if [ "$#" -eq  "0" ]
+    then
+      BRANCH=$(gum input --placeholder "Digite o nome da branch")
+  else
+      BRANCH=$1
+  fi
+
+  git fetch upstream
+
+  git checkout -b $BRANCH upstream/$BRANCH
+
+  git push --set-upstream origin $BRANCH
 }
 
 
