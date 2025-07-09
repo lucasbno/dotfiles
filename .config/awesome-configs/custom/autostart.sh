8f2322012ac85384e879ae05ae5edc9c78f34416
Lucas Bueno <lucasbno@proton.me>
2025-07-09 16:26:03 -0300

    add missing files yadm

---
+++ .config/awesome-configs/custom/autostart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+nitrogen --restore &
+# if ! pgrep -x pipewire > /dev/null; then
+#     pipewire &
+#     pipewire-pulse &
+# fi
+pkill -x pipewire
+pkill -x pipewire-pulse
+pipewire &
+pipewire-pulse &
+xset r rate 220 40 &
+xmodmap ~/.Xmodmap &
+xcape -e 'Hyper_L=Escape' -t 100 &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# picom &
+xinput set-prop 'pointer:Compx VXE NordicMouse 1K Dongle' 'libinput Accel Profile Enabled' 0 1 &
+xinput set-prop 'pointer:Compx VXE NordicMouse 1K Dongle' 'libinput Accel Speed' 0.7
