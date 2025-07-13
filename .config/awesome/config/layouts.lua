19c65847f6ca1b500ee711080fffc44f934aabc5
Lucas Bueno <lucasbno@proton.me>
2025-07-13 03:51:01 -0300

    layout

--- .config/awesome/config/layouts.lua
+++ .config/awesome/config/layouts.lua
@@ -4,7 +4,7 @@ local awful = require "awful"
 local function set_layouts()
   tag.connect_signal("request::default_layouts", function()
     awful.layout.append_default_layouts {
-      awful.layout.suit.tile.left,
+      awful.layout.suit.tile.right,
       awful.layout.suit.floating,
       awful.layout.suit.fair,
       -- bling.layout.mstab
